84d00200b21ff2e34c20582d37f0f09b0eadc206
khoacutene <dangtienkhoa11192005@gmail.com>
2024-09-11 00:32:04 +0700

    update-1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const tienDien_1 = 500;
 const tienDien_2 = 650;
 const tienDien_3 = 850;
@@ -23,34 +24,30 @@ function calculate () {
         price = price_1 + price_2
 
     } else if (50 < distance && distance <= 100) {
-        price_1 = (1 * tienDien_1)
-        price_2 = (distance - 1) * tienDien_1
-        price_3 = (distance - 50) * tienDien_2
-        price =  price_1 + price_2 + price_3
+        price_1 = (distance - (distance - 50)) * tienDien_1
+        price_2 = (distance - 50) * tienDien_2
+        price =  price_1 + price_2 
 
     } else if (100 < distance && distance <= 200) {
-        price_1 = (1 * tienDien_1)
-        price_2 = (distance - 1) * tienDien_1
-        price_3 = (distance - 50) * tienDien_2
-        price_4 = (distance - 100) * tienDien_3
-        price = price_1 + price_2 + price_3 + price_4
+        price_1 = (distance - (distance - 50)) * tienDien_1
+        price_2 = (distance -(distance - 50)) * tienDien_2
+        price_3 = (distance - 100) * tienDien_3
+        price = price_1 + price_2 + price_3
 
     } else if (200 < distance && distance <= 350 ) {
-        price_1 = (1 * tienDien_1)
-        price_2 = (distance - 1) * tienDien_1
-        price_3 = (distance - 50) * tienDien_2
-        price_4 = (distance - 100) * tienDien_3
-        price_5 = (distance - 150) * tienDien_4
-        price = price_1 + price_2 + price_3 + price_4 + price_5
+        price_1 = (distance - (distance - 50)) * tienDien_1
+        price_2 = (distance - (distance - 50)) * tienDien_2
+        price_3 = (distance - (distance - 100)) * tienDien_3
+        price_4 = (distance - 200) * tienDien_4
+        price = price_1 + price_2 + price_3 + price_4
 
     } else if (350 > distance) {
-        price_1 = (1 * tienDien_1)
-        price_2 = (distance - 1) * tienDien_1
-        price_3 = (distance - 50) * tienDien_2
-        price_4 = (distance - 100) * tienDien_3
-        price_5 = (distance - 150) * tienDien_4
-        price_6 = (distance - 250) * tienDien_5
-        price = price_1 + price_2 + price_3 + price_4 + price_5 + price_6
+        price_1 = (distance - (distance - 50)) * tienDien_1
+        price_2 = (distance - (distance - 50)) * tienDien_2
+        price_3 = (distance - (distance - 100)) * tienDien_3
+        price_4 = (distance - (distance - 150)) * tienDien_4
+        price_5 = (distance - 350) * tienDien_4
+        price = price_1 + price_2 + price_3 + price_4 + price_5
 
     } else {
         alert("Làm ơn nhập thông tin")
@@ -58,71 +55,30 @@ function calculate () {
 
     const formatter = new   Intl.NumberFormat("vi-VN", {currency: "VND", style: "currency"});
 
+    const pInHoTen = document.getElementById("pInHoTen")
+    const ten = document.getElementById("ten")
+
     const pTien = document.getElementById("pTien");
+    pTien.innerHTML = `Họ tên: ` + ten.value + `Tiền điện là: ` + formatter.format(price);
 
-    pTien.innerHTML =  formatter.format(price);
 
 }
 
-// Nhà Dân
-const nDanHoaDon = 4.5
-const nDanDichVu = 20.5
-const nDanThueKenh = 7.5
 
-// Doanh Nghiệp
-const dNghiepHoaDon = 15
-const dNghiepDichVu = 75
-const dNghiepDichVu_them = 75
-const dNghiepThueKenh = 50
+// Nhà dân
+const nDHoaDon = 4.5;
+const nDDichVu = 20.5;
+const nDKenh = 7.5;
 
-const getTypeCap = () => {
-    const dan = document.getElementById("nhaDan")
-    const nghiep = document.getElementById("doanhNghiep")
+// Doanh nghiệp
+const dNHoaDon = 15;
+const dNKenh = 50;
+const dNDichVu = 75;
+const dNDichVuThem = 5;
 
-    let type = "";
-
-    if (dan.checked) {
-        type = "NHA_DAN";
-    } else if (nghiep.checked) {
-        type = "DOANH_NGHIEP";
-    } 
-    return type;
-}
-
-const calculateDetail = (NHADAN, DOANHNGHIEP) => {
+const tinhTienCap = () => {
     let price = 0
     let price_1 = 0
-    let price_2 = 0
     let price_3 = 0
-
-    if (NHADAN) {
-        price_1 = (nDanDichVu * 1) * NHADAN
-        price_2 = (nDanHoaDon * 1) 
-        price_3 = NHADAN * nDanThueKenh
-        price = price_1 + price_2 + price_3
-    }
-    if (DOANHNGHIEP) {
-        price_1 = dNghiepHoaDon * 1
-        price_2 = dNghiepDichVu * 1
-        price_3 = DOANHNGHIEP * dNghiepThueKenh
-        price = price_1 + price_2 + price_3
-    }
-    const formatter = new   Intl.NumberFormat("vi-VN", {currency: "VND", style: "currency"});
-
-    document.getElementById("pThongBao").innerHTML = formatter.format(price  );
-}
-
-function calculate() {
-    const type = getTypeCap();
-    const NHADAN = document.getElementById("txtKhachHang").value
-    const DOANHNGHIEP = document.getElementById("txtKenh").value
-
-    switch (type) {
-        case "NHA_DAN":
-            calculateDetail (NHADAN, DOANHNGHIEP, nDanHoaDon,nDanDichVu, nDanThueKenh);
-            break;
-        case "DOANH_NGHIEP":
-            calculateDetail(NHADAN, DOANHNGHIEP, dNghiepHoaDon, dNghiepDichVu, dNghiepThueKenh);
-            break;
-    }
+    let price_4 = 0
 }
